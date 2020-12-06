db19a055e07d7cc61c27f932ebec02aae02b3b0d
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-12-06 19:27:08 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,10 +2,7 @@ require 'pry'
 
 def find_item_by_name_in_collection(name, collection)
   collection.each do |list|
-    list.each do |key, item_name|
-      if item_name == name
-        return list
-      end
+    matching_item = list.each { |key, item_name| item_name == name }
     end
   end
 end
