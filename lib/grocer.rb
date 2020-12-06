99205ffe3e25df038ca7f7bc77db8c6403796ca0
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-12-06 19:48:21 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -3,6 +3,7 @@ require 'pry'
 def find_item_by_name_in_collection(name, collection)
   collection.each do |list|
     list.select do |key, item_name|
+      binding.pry
       if item_name == name
         return list
       else
