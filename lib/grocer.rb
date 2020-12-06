c18c7532bf1527ee064f414195bab6a9e04c1784
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-12-06 21:01:48 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -11,39 +11,14 @@ def find_item_by_name_in_collection(name, collection)
   nil
 end
 
-# def consolidate_cart(cart)
-#   index = 0
-#   count = 0
-#
-#   cart.each_with_object([]) do |product, final_cart|
-#     new_item = find_item_by_name_in_collection(product, final_cart)
-#     if new_item
-#       final_cart[index] = new_item
-#     end
-#     binding.pry
-#   end
-# end
-
 def consolidate_cart(cart)
   index = 0
-  new_cart = []
 
-  cart.each do |grocery_item|
-    current_item = find_item_by_name_in_collection(grocery_item[:item], new_cart)
-    if current_item
-      binding.pry
-      new_cart_index = 0
-      new_cart.each do |new_cart_item|
-        if new_cart_item[:item] === current_item[:item]
-          new_cart_item[:count] += 1
-        end
-        new_cart_index += 1
-      end
-    else
-      grocery_item[:count] = 1
-      new_cart << grocery_item
+  cart.each_with_object([]) do |product, final_cart|
+    new_item = find_item_by_name_in_collection(product, final_cart)
+    if new_item
+      final_cart[index] = new_item
     end
-    index += 1
+    binding.pry
   end
-  new_cart
 end
