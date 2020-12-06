12170b6d0fa8dab16371379db37c5029ba5affcf
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-12-06 19:20:36 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -5,6 +5,7 @@ def find_item_by_name_in_collection(name, collection)
     list.each do |key, item_name|
       if item_name == name
         return item_name
+        binding.pry
       end
     end
   end
