1e2080fcb08d6cb6b045d2685b2f147b8dafe414
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-12-06 21:26:20 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -14,6 +14,7 @@ end
 def consolidate_cart(cart)
   cart.each_with_object([]) do |product, final_cart|
     new_item = find_item_by_name_in_collection(product, final_cart)
+    binding.pry
     if !new_item
       product[:count] = 1
       final_cart << product
