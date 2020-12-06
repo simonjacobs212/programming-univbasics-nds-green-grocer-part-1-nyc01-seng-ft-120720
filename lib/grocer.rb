e6705949f1fd641e3db1abc8f98d67ea7ee43720
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-12-06 21:29:21 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,14 +1,7 @@
 require 'pry'
 
 def find_item_by_name_in_collection(name, collection)
-  collection.each do |list|
-    list.select do |key, item_name|
-      if item_name == name
-        return list
-      end
-    end
-  end
-  nil
+  collection.any?(name)
 end
 
 def consolidate_cart(cart)
