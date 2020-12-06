1284beee29e260a24a8f164fcf729dc2f19f32cd
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-12-06 19:10:20 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,12 +2,11 @@ require 'pry'
 
 def find_item_by_name_in_collection(name, collection)
   collection.each do |list|
-    matching_item = list.each_with_object({}) do |item, item_name|
+    list.each_with_object({}) do |item, item_name|
       item_name == name
       binding.pry
     end
   end
-  matching_item
 end
 
 def consolidate_cart(cart)
